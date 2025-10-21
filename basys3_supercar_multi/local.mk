2435baf51381c73d60b98cd784dcb2a968025b3d
Mirko Mariotti <mirko@mirkomariotti.it>
2025-10-21 08:59:32 +0200

    :sparkles: KITT examples

---
+++ basys3_supercar_multi/local.mk
@@ -0,0 +1,12 @@
+WORKING_DIR=working_dir
+CURRENT_DIR=$(shell pwd)
+BOARD=basys3
+SHOWARGS=-dot-detail 5
+BASM_ARGS=-d -disable-dynamical-matching -bo $(WORKING_DIR)/bondmachine.bcof -chooser-min-word-size -chooser-force-same-name -create-mapfile basys3_maps.json
+BASM_LIB=library
+VERILOG_OPTIONS=-comment-verilog
+CLUSTER_SPEC=cluster.json
+USE_BONDIRECT=true
+BONDIRECT_MAPFILE=edgenode_maps.json
+BONDIRECT_MESH=home.json
+CLUSTER=true
