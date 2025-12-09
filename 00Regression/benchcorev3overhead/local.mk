7c728fcc121640ebcb72862a1ef3b2ee76d9f24f
Mirko Mariotti <mirko@mirkomariotti.it>
2025-12-09 16:58:00 +0100

    :stethoscope: (benchcorev3overhead) new regression data

--- 00Regression/benchcorev3overhead/local.mk
+++ 00Regression/benchcorev3overhead/local.mk
@@ -5,7 +5,7 @@ SOURCE_BASM=test.basm
 BASM_ARGS=-disable-dynamical-matching -chooser-min-word-size -chooser-force-same-name
 MAPFILE=zedboard_maps.json
 SHOWARGS=-dot-detail 5
-EXTRACLEAN=expression.c sicv2 *.seq num_outputs.txt bondmachine.png statistics.json source.mk out.basm
+EXTRACLEAN=sicv2 num_outputs.txt bondmachine.png statistics.json source.mk out.basm
 include bmapi.mk
 include deploy.mk
 include simbatch.mk
